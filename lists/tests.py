c1b9bbacc95010dd33f73f77da74ca9b8188d260
n-freman <smiron1703@gmail.com>
2023-10-25 09:51:31 +0500

    Adjust model so that items are associated with different lists

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 
-from lists.models import Item
+from lists.models import Item, List
 from lists.views import home_page
 
 
@@ -13,29 +13,38 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
     def test_uses_list_template(self):
-        response = self.client.get('/lists/the-only-list-in-the-world')
+        response = self.client.get('/lists/the-only-list-in-the-world/')
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1') # type: ignore
-        Item.objects.create(text='itemey 2') # type: ignore
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_) # type: ignore
+        Item.objects.create(text='itemey 2', list=list_) # type: ignore
 
-        response = self.client.get('/')
+        response = self.client.get('/lists/the-only-list-in-the-world/')
 
         self.assertIn('itemey 1', response.content.decode()) # type: ignore
         self.assertIn('itemey 2', response.content.decode()) # type: ignore
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item' # type: ignore
+        first_item.list = list_ # type: ignore
         first_item.save()
         
         second_item = Item()
         second_item.text = 'Item the second' # type: ignore
+        second_item.list = list_ # type: ignore
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all() # type: ignore
         self.assertEqual(saved_items.count(), 2)
@@ -43,7 +52,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -68,5 +79,5 @@ class NewListTest(TestCase):
 
     def test_redirects_after_POST(self):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
-        self.assertRedirects(response, 'lists/the-only-list-in-the-world/')
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
