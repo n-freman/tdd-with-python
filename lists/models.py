2fb8c2cb7422c2f55527b9bb379e52a1f5f63190
n-freman <smiron1703@gmail.com>
2023-10-26 10:39:23 +0500

    new URL + view for adding to existing lists. FT passes :-)

--- lists/models.py
+++ lists/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class List(models.Model):
-    pass
+    objects: models.Manager # for pyright
 
 
 class Item(models.Model):
+    objects: models.Manager # for pyright
     text = models.TextField(default='')
     list = models.ForeignKey(
         List,
         default=None,
         on_delete=models.CASCADE
     )
-
