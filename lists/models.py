23a97eae3bcb79b2f78da314ab7186b159005afc
n-freman <smiron1703@gmail.com>
2023-11-04 10:23:19 +0500

    use get_absolute_url on List model to DRY urls in views

--- lists/models.py
+++ lists/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.shortcuts import reverse
 
 
 class List(models.Model):
     objects: models.Manager # for pyright
+
+    def get_absolute_url(self):
+        return reverse('view_list', args=[self.id])
 
 
 class Item(models.Model):
@@ -13,3 +17,4 @@ class Item(models.Model):
         default=None,
         on_delete=models.CASCADE
     )
+
