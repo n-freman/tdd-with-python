14868c6d3737627581c3e0faf4fad8f9b40cf654
n-freman <smiron1703@gmail.com>
2023-11-13 10:37:15 +0500

    add extra tests for js

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,4 +1,9 @@
-const initialize = () => {	
+window.Superlists = {};
+
+window.Superlists.initialize = () => {	
+	$('input[name="text"]').on('focus', () => {
+		$('.has-error').hide();
+	})
 	$('input[name="text"]').on('keypress', () => {
 		$('.has-error').hide();
 	});
