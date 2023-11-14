2d32964c17aa9574a30bd192042e99fb8381fed1
n-freman <smiron1703@gmail.com>
2023-11-14 10:25:40 +0500

    add navbar, logging, accounts app

--- superlists/settings.py
+++ superlists/settings.py
@@ -125,3 +125,19 @@ STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
